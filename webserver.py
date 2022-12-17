4df64c0a95b47f37b64f8a98ce3a0b26132c30b6
Kerwin1202 <958686489@qq.com>
2022-12-17 20:23:57 +0800

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -1,33 +1,66 @@
 import subprocess
-from flask import Flask, request, Response
+from flask import Flask, request, Response, jsonify, render_template
+import os
+from urllib.parse import urljoin
 
-server = Flask(__name__)
+server = Flask(__name__, template_folder='/opt/app')
 
 SINGLEFILE_EXECUTABLE = '/node_modules/single-file/cli/single-file'
 BROWSER_PATH = '/opt/google/chrome/google-chrome'
 BROWSER_ARGS = '["--no-sandbox"]'
 
+BASE_DIR = './htmls'
 
-@server.route('/', methods=['POST'])
+
+def get_html_path(key, name):
+    return f'{BASE_DIR}/{key}/{name}.html'
+
+
+@server.route('/dl/<key>/<name>.html')
+def show_html(key, name):
+    return render_template(get_html_path(key, name))
+
+
+@server.route('/', methods=['GET'])
 def singlefile():
-    url = request.form.get('url')
+    print(request.url)
+    url = request.args.get('url')
+    key = request.args.get('key')
+    name = request.args.get('name')
+    refresh = request.args.get('refresh', False)
+    if key and name and not refresh and os.path.exists(get_html_path(key, name)):
+        response = {'url': urljoin(request.base_url, f'/dl/{key}/{name}.html')}
+        return jsonify(response)
+
     if url:
         p = subprocess.Popen([
             SINGLEFILE_EXECUTABLE,
             '--browser-executable-path=' + BROWSER_PATH,
             "--browser-args='%s'" % BROWSER_ARGS,
-            request.form['url'],
+            url,
             '--dump-content',
-            ],
+        ],
             stdout=subprocess.PIPE)
     else:
         return Response('Error: url parameter not found.',
                         status=500)
     singlefile_html = p.stdout.read()
-    return Response(
-        singlefile_html,
-        mimetype="text/html",
-    )
+
+    key = request.args.get('key')
+    name = request.args.get('name')
+    if key and name:
+        folder_path = f'{BASE_DIR}/{key}'
+        if not os.path.exists(folder_path):
+            os.mkdir(folder_path)
+        with open(get_html_path(key, name), 'wb') as f:
+            f.write(singlefile_html)
+        response = {'url': urljoin(request.base_url, f'/dl/{key}/{name}.html')}
+        return jsonify(response)
+    else:
+        return Response(
+            singlefile_html,
+            mimetype="text/html",
+        )
 
 
 if __name__ == '__main__':
